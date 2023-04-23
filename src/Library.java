187d7feca83d7d9d9c9f8c66cdef3dadcbd90863
Amirhossein <Amirmemool12@gmail.com>
2023-04-23 17:18:43 -0400

    impelement library class

---
+++ src/Library.java
@@ -0,0 +1,36 @@
+public class Library {
+    private String id;
+    private String name;
+    private String year;
+    private int numDesk;
+    private String address;
+
+    public Library(String id, String name, String year, int numDesk, String address) {
+        this.id = id;
+        this.name = name;
+        this.year = year;
+        this.numDesk = numDesk;
+        this.address = address;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public int getNumDesk() {
+        return numDesk;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+    
+}
