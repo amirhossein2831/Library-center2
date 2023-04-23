bffaaf843cf9f9dd65a26efb8c5b3ab605a6f762
Amirhossein <Amirmemool12@gmail.com>
2023-04-23 17:15:40 -0400

    implement parser class

---
+++ src/Parser.java
@@ -0,0 +1,8 @@
+public class Parser {
+    public void parsCommand(String input) {
+        String[] command = input.split("#");
+        String[] args = command[1].split("\\|");
+        switch (command) {
+        }
+    }
+}
