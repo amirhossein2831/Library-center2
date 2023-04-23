d445597d96a60702bcf029b4b0f184b2852cc27f
Amirhossein <Amirmemool12@gmail.com>
2023-04-23 17:34:32 -0400

    add addlibrary in center class

--- src/Center.java
+++ src/Center.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Center {
+import java.util.HashMap;
+
+public class Center {
+    private HashMap<String, Library> libraries;
+
+    public Center() {
+        libraries = new HashMap<>();
+    }
+    //TODO this need more condition about the admin     
+    public String addLibrary(Library library) {
+        if (libraries.get(library.getId()) != null) {
+            return "duplicate-id";
+        }
+        libraries.put(library.getId(), library);
+        return "success";
+    }
 }
