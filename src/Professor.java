c9e6abfa762ccedb9357cae2c9bada194a9ddc4a
Amirhossein <Amirmemool12@gmail.com>
2023-04-25 16:10:11 -0400

    change the id and pass from the professor  to user

--- src/Professor.java
+++ src/Professor.java
@@ -1,18 +1,5 @@
 public class Professor extends User{
-    private String id;
-    private String pass;
-
     public Professor( String id, String pass,String firstName, String lastName, String nationalId, String year, String address) {
-        super(firstName, lastName, nationalId, year, address);
-        this.id = id;
-        this.pass = pass;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getPass() {
-        return pass;
+        super(id,pass,firstName, lastName, nationalId, year, address);
     }
 }
