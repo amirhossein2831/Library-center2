806f6c063e40a5e0b24f92edfab3429be74c177e
Amirhossein <Amirmemool12@gmail.com>
2023-04-25 14:08:43 -0400

    add main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        Parser parser = new Parser();
+        while (!input.equals("finish")) {
+            input = scanner.nextLine();
+            parser.parsCommand(input);
+        }
     }
 }
