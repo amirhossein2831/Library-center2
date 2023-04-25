196f92a5510ccf55cabac27c416edfd4e1503aae
Amirhossein <Amirmemool12@gmail.com>
2023-04-25 16:08:38 -0400

    change the id and pass from the student to user

--- src/User.java
+++ src/User.java
@@ -1,18 +1,28 @@
 public class User {
+    private String id;
+    private String pass;
     private String firstName;
     private String lastName;
     private String nationalId;
     private String year;
     private String address;
 
-    public User(String firstName, String lastName, String nationalId, String year, String address) {
+    public User(String id,String pass,String firstName, String lastName, String nationalId, String year, String address) {
+        this.id = id;
+        this.pass = pass;
         this.firstName = firstName;
         this.lastName = lastName;
         this.nationalId = nationalId;
         this.year = year;
         this.address = address;
     }
+    public String getId() {
+        return id;
+    }
 
+    public String getPass() {
+        return pass;
+    }
     public String getFirstName() {
         return firstName;
     }
