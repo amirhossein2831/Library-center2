38a1f18227fa34b096b86eab8611df0a046cc44b
Amirhossein <Amirmemool12@gmail.com>
2023-04-25 15:36:13 -0400

    pass the arg in parser for gangine method

--- src/Parser.java
+++ src/Parser.java
@@ -33,7 +33,7 @@ public class Parser {
                 addThesis(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8]);
                 break;
             case "add-ganjineh-book":
-                addGanjine();
+                addGanjine(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9]);
                 break;
         }
     }
