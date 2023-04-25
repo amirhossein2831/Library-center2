93457242d491475bd0c38f69d6d2a702f79aae19
Amirhossein <Amirmemool12@gmail.com>
2023-04-25 16:09:10 -0400

    change the id and pass from the staff  to user

--- src/Staff.java
+++ src/Staff.java
@@ -1,18 +1,6 @@
 public class Staff extends User{
-    private String id;
-    private String pass;
-
     public Staff( String id, String pass,String firstName, String lastName, String nationalId, String year, String address) {
-        super(firstName, lastName, nationalId, year, address);
-        this.id = id;
-        this.pass = pass;
+        super(id,pass,firstName, lastName, nationalId, year, address);
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public String getPass() {
-        return pass;
-    }
 }
