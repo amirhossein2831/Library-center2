1269ed78cb34d614622d1f7deb416bd7805407ba
Amirhossein <Amirmemool12@gmail.com>
2023-04-25 16:10:47 -0400

    change the id and pass from the manager to user

--- src/Manager.java
+++ src/Manager.java
@@ -1,23 +1,10 @@
 public class Manager extends User {
-    private String id;
-    private String pass;
     private String libraryId;
 
     public Manager(String id, String pass,String firstName, String lastName, String nationalId, String year, String address,String libraryId) {
-        super(firstName, lastName, nationalId, year, address);
-        this.id = id;
-        this.pass = pass;
+        super(id,pass,firstName, lastName, nationalId, year, address);
         this.libraryId = libraryId;
     }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getPass() {
-        return pass;
-    }
-
     public String getLibraryId() {
         return libraryId;
     }
