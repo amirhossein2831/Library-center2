db4d4dc7ee6ebbabee946ac3d087a731476d0ecd
Amirhossein <Amirmemool12@gmail.com>
2023-04-25 15:41:34 -0400

    make SellingBook class

---
+++ src/SellingBook.java
@@ -0,0 +1,19 @@
+public class SellingBook extends Book{
+    private String price;
+    private String discount;
+
+    public SellingBook(String id, String subject,
+                       String authorName, String categoryId, String libraryId, String publisher, int number, String year, String price, String discount) {
+        super(id, subject, authorName, categoryId, libraryId, publisher, number, year);
+        this.price = price;
+        this.discount = discount;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public String getDiscount() {
+        return discount;
+    }
+}
