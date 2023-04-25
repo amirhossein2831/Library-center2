b32b8c2e21df1b8b825b9f163e0f60268d78622c
Amirhossein <Amirmemool12@gmail.com>
2023-04-25 15:31:09 -0400

    make ganjinebook class

--- src/GanjineBook.java
+++ src/GanjineBook.java
@@ -1,2 +1,12 @@
-public class GanjineBook {
+public class GanjineBook extends Book {
+    private String donor;
+
+    public GanjineBook(String id, String subject, String authorName, String categoryId, String libraryId, String publisher, int number, String year, String donor) {
+        super(id, subject, authorName, categoryId, libraryId, publisher, number, year);
+        this.donor = donor;
+    }
+
+    public String getDonor() {
+        return donor;
+    }
 }
