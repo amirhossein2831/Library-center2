f4a746c723132005897d60b1390adcc3441279ae
Amirhossein <Amirmemool12@gmail.com>
2023-05-06 17:13:02 -0400

    add buys field to professor  class

--- src/Professor.java
+++ src/Professor.java
@@ -1,5 +1,13 @@
+import java.util.ArrayList;
+
 public class Professor extends User {
+    private ArrayList<SellingBook> buys;
     public Professor(String id, String pass, String firstName, String lastName, String nationalId, String year, String address) {
         super(id, pass, firstName, lastName, nationalId, year, address);
+        buys = new ArrayList<>();
     }
+    public void buy(SellingBook sellingBook) {
+        buys.add(sellingBook);
+    }
+
 }
