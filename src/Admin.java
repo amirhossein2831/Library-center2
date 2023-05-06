ed09dc15845d3715afc1629786694fc99d60e73f
Amirhossein <Amirmemool12@gmail.com>
2023-05-06 17:13:38 -0400

    add buys field to Admin  class

--- src/Admin.java
+++ src/Admin.java
@@ -1,5 +1,13 @@
+import java.util.ArrayList;
+
 public class Admin extends User {
+    private ArrayList<SellingBook> buys;
     public Admin(String id, String pass, String firstName, String lastName, String nationalId, String year, String address) {
         super(id, pass, firstName, lastName, nationalId, year, address);
+        buys = new ArrayList<>();
     }
+    public void buy(SellingBook sellingBook) {
+        buys.add(sellingBook);
+    }
+
 }
