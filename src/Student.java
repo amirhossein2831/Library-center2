9f2f0d4157b242575fbc939aefab5e0822e7f7a6
Amirhossein <Amirmemool12@gmail.com>
2023-05-06 17:09:50 -0400

    add buys field to Student class

--- src/Student.java
+++ src/Student.java
@@ -1,5 +1,15 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Student extends User {
+    private ArrayList<SellingBook> buys;
+
     public Student(String id, String pass, String firstName, String lastName, String nationalId, String year, String address) {
         super(id, pass, firstName, lastName, nationalId, year, address);
+        buys = new ArrayList<>();
+    }
+
+    public void buy(SellingBook sellingBook) {
+        buys.add(sellingBook);
     }
 }
