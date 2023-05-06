2870bec102146516e3e52f3837614d1123ad7c2e
Amirhossein <Amirmemool12@gmail.com>
2023-05-06 17:12:11 -0400

    add buys field to staff class

--- src/Staff.java
+++ src/Staff.java
@@ -1,6 +1,14 @@
+import java.util.ArrayList;
+
 public class Staff extends User {
+    private ArrayList<SellingBook> buys;
     public Staff(String id, String pass, String firstName, String lastName, String nationalId, String year, String address) {
         super(id, pass, firstName, lastName, nationalId, year, address);
+        buys = new ArrayList<>();
+    }
+
+    public void buy(SellingBook sellingBook) {
+        buys.add(sellingBook);
     }
 
 }
