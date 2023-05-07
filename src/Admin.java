29eda81c643f8d045eefaf77eff17951f79c6231
Amirhossein <Amirmemool12@gmail.com>
2023-05-07 12:20:46 -0400

    delete the searchUser in Admin

--- src/Admin.java
+++ src/Admin.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.HashSet;
 
-public class Admin extends User implements Buy,SearchUser {
+public class Admin extends User implements Buy {
     private ArrayList<SellingBook> buys;
     public Admin(String id, String pass, String firstName, String lastName, String nationalId, String year, String address) {
         super(id, pass, firstName, lastName, nationalId, year, address);
@@ -10,19 +10,4 @@ public class Admin extends User implements Buy,SearchUser {
     public void buy(SellingBook sellingBook) {
         buys.add(sellingBook);
     }
-
-    @Override
-    public HashSet<String> searchUser(ArrayList<User> users,String key) {
-        HashSet<String> value = new HashSet<>();
-        for (User user : users) {
-            if (user.getFirstName().equalsIgnoreCase(key)) {
-                value.add(user.getId());
-            }
-            else if (user.getLastName().equalsIgnoreCase(key)) {
-                value.add(user.getId());
-            }
-        }
-        return value;
-    }
-
 }
