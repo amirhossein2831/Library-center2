46a47d5a14e2ed1f9c1981c8555987ed9f35cdb6
Amirhossein <Amirmemool12@gmail.com>
2023-05-08 08:31:13 -0400

    change the parent field of category from string to arrayList and another field

--- src/Category.java
+++ src/Category.java
@@ -1,12 +1,17 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Category {
     private final String id;
     private final String name;
     private final String parentId;
-
+    private ArrayList<String> subs;
     public Category(String id, String name, String parentId) {
         this.id = id;
         this.name = name;
         this.parentId = parentId;
+        subs = new ArrayList<>();
     }
 
     public String getId() {
@@ -19,5 +24,12 @@ public class Category {
 
     public String getParentId() {
         return parentId;
+    }
+
+    public ArrayList<String> getSubs() {
+        return subs;
+    }
+    public void setSubs(String sub) {
+        this.subs.add(sub);
     }
 }
