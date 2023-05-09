41aceeb1d4755a39e0c288730942771cc9ca070d
Amirhossein <Amirmemool12@gmail.com>
2023-05-09 15:05:49 -0400

    create rule class  and its field and getter setter

--- src/Rule.java
+++ src/Rule.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class rULE {
+import java.util.HashMap;
+
+public class Rule {
+    private User user;
+    private HashMap<String, User> users;
+    private Library library;
+    private HashMap<String, Library> libraries;
+    private Resource resource;
+  
+
+    public User getUser() {
+        return user;
+    }
+    public Library getLibrary() {
+        return library;
+    }
+    public Resource getResource() {
+        return resource;
+    }
+    public void setUsers(HashMap<String, User> users) {
+        this.users = users;
+    }
+    public void setLibraries(HashMap<String, Library> libraries) {
+        this.libraries = libraries;
+    }
 }
