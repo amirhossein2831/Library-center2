f3c652ab17eca46cdc4a112b9af0f993ad63dfd3
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-03-14 01:34:55 +0330

    implement the authentication of the manager

--- src/com/Library/Auth/Auth.java
+++ src/com/Library/Auth/Auth.java
@@ -5,6 +5,7 @@ import com.Library.Compunent.Exception.NotFoundException;
 import com.Library.Compunent.Exception.PermissionDeniedException;
 import com.Library.Models.LibraryCenter;
 import com.Library.Models.Users.Admin;
+import com.Library.Models.Users.Manager;
 
 public class Auth {
     public static void adminAuth(String id, String pass) {
@@ -15,6 +16,26 @@ public class Auth {
         }
         if (!LibraryCenter.getUsers().get(id).getPass().equals(pass)) {
             throw new InvalidPassException();
+        }
+    }
+
+    public static void managerAuth(String id, String pass) {
+        if (LibraryCenter.getUsers().get(id) == null) {
+            throw new NotFoundException();
+        } else if (!LibraryCenter.getUsers().get(id).getPass().equals(pass)) {
+            throw new InvalidPassException();
+        }
+        if (!(LibraryCenter.getUsers().get(id) instanceof Manager)) {
+            throw new PermissionDeniedException();
+        }
+    }
+
+    public static void libraryManagerAuth(String id,String libraryId) {
+        if (LibraryCenter.getLibraries().get(libraryId) == null) {
+            throw new NotFoundException();
+        }
+        if (!((Manager) LibraryCenter.getUsers().get(id)).getLibraryId().equals(libraryId)) {
+            throw new PermissionDeniedException();
         }
     }
 }
--- src/com/Library/Controller/ManagerController.java
+++ src/com/Library/Controller/ManagerController.java
@@ -1,45 +1,48 @@
 package com.Library.Controller;
 
+import com.Library.Auth.Auth;
 import com.Library.Compunent.Rule.Rule;
 import com.Library.Models.Library;
 import com.Library.Models.LibraryCenter;
 import com.Library.Models.Resources.*;
+import com.Library.Models.Users.User;
 
 public class ManagerController extends BaseController {
 
     public String addBook(String[] args) {
         Book book = new Book(args[2], args[3], args[4], args[8], args[9], args[5], Integer.parseInt(args[7]), args[6]);
-        Rule rule = new Rule(args[0], args[1], book.getLibraryId(), LibraryCenter.getUsers(), LibraryCenter.getLibraries());
+        Auth.libraryManagerAuth(args[0],book.getLibraryId());
         return checkResource(book);
     }
 
     public String addThesis(String[] args) {
         Thesis thesis = new Thesis(args[2], args[3], args[4], args[7], args[8], args[4], args[6]);
-        Rule rule = new Rule(args[0], args[1], thesis.getLibraryId(), LibraryCenter.getUsers(), LibraryCenter.getLibraries());
+        Auth.libraryManagerAuth(args[0],thesis.getLibraryId());
         return checkResource(thesis);
     }
 
     public String addGanjine(String[] args) {
         GanjineBook ganjineBook = new GanjineBook(args[2], args[3], args[4], args[8], args[9], args[5], args[6], args[7]);
-        Rule rule = new Rule(args[0], args[1], ganjineBook.getLibraryId(), LibraryCenter.getUsers(), LibraryCenter.getLibraries());
+        Auth.libraryManagerAuth(args[0],ganjineBook.getLibraryId());
         return checkResource(ganjineBook);
     }
 
     public String addSellingBook(String[] args) {
         SellingBook sellingBook = new SellingBook(args[2], args[3], args[4], args[10], args[11], args[5], Integer.parseInt(args[7]), args[6], args[8], args[9]);
-        Rule rule = new Rule(args[0], args[1], sellingBook.getLibraryId(), LibraryCenter.getUsers(), LibraryCenter.getLibraries());
+        Auth.libraryManagerAuth(args[0],sellingBook.getLibraryId());
         return checkResource(sellingBook);
     }
 
     public String removeResource(String[] args) {
-        Rule rule = new Rule(args[0], args[1], args[3], LibraryCenter.getUsers(), LibraryCenter.getLibraries());
-        if (rule.getLibrary().getResources().get(args[2]) == null) {
+        Auth.libraryManagerAuth(args[0],args[3]);
+        Library library = LibraryCenter.getLibraries().get(args[3]);
+        if (library.getResources().get(args[2]) == null) {
             return "not-found";
         }
-        if (rule.getLibrary().getBorrows().get(args[2]) != null) {
+        if (library.getBorrows().get(args[2]) != null) {
             return "not-allowed";
         }
-        rule.getLibrary().getResources().remove(args[2]);
+        library.getResources().remove(args[2]);
         return "success";
     }
 
