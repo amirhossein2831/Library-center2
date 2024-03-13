56781af004f138c63560f0db8a8287bf7c4dcb13
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-03-13 15:28:24 +0330

    update the parser logic and make it compatible with router

--- src/com/Library/Compunent/Parser/Parser.java
+++ src/com/Library/Compunent/Parser/Parser.java
@@ -1,8 +1,10 @@
 package com.Library.Compunent.Parser;
 
+import com.Library.Compunent.Exception.Route;
+import com.Library.Compunent.Reflection.Reflection;
+import com.Library.Controller.Center;
 import com.Library.Models.Borrow;
 import com.Library.Models.Category;
-import com.Library.Controller.Center;
 import com.Library.Models.Library;
 import com.Library.Models.Resources.Book;
 import com.Library.Models.Resources.GanjineBook;
@@ -13,214 +15,144 @@ import com.Library.Models.Users.Professor;
 import com.Library.Models.Users.Staff;
 import com.Library.Models.Users.Student;
 
+import java.lang.reflect.InvocationTargetException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Parser {
     private final Center center;
+    private final Route router;
 
     public Parser() {
         center = new Center();
+        router = new Route();
     }
 
-    public void parsCommand(String input) throws ParseException {
+    public void parsCommand(String input) throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
         String[] command = input.split("#");
-        if (command.length == 1) {
+        if (command.length < 2) {
+            System.out.println("invalid input");
             return;
         }
         String[] args = command[1].split("\\|");
-        switch (command[0]) {
-            case "add-library":
-                addLibrary(args[0], args[1], args[2], args[3], args[4], Integer.parseInt(args[5]), args[6]);
-                break;
-            case "add-category":
-                addCategory(args[0], args[1], args[2], args[3], args[4]);
-                break;
-            case "add-student":
-                addStudent(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8]);
-                break;
-            case "add-staff":
-                addStaff(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9]);
-                break;
-            case "add-manager":
-                addManager(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9]);
-                break;
-            case "remove-user":
-                removeUser(args[0], args[1], args[2]);
-                break;
-            case "add-book":
-                addBook(args[0], args[1], args[2], args[3], args[4], args[5], args[6], Integer.parseInt(args[7]), args[8], args[9]);
-                break;
-            case "add-thesis":
-                addThesis(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8]);
-                break;
-            case "add-ganjineh-book":
-                addGanjine(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9]);
-                break;
-            case "add-selling-book":
-                addSellingBook(args[0], args[1], args[2], args[3], args[4], args[5], args[6], Integer.parseInt(args[7]), args[8], args[9], args[10], args[11]);
-                break;
-            case "remove-resource":
-                removeResource(args[0], args[1], args[2], args[3]);
-                break;
-            case "borrow":
-                borrow(args[0], args[1], args[2], args[3], args[4], args[5]);
-                break;
-            case "return":
-                returning(args[0], args[1], args[2], args[3], args[4], args[5]);
-                break;
-            case "buy":
-                buy(args[0], args[1], args[2], args[3]);
-                break;
-            case "read":
-                read(args[0], args[1], args[2], args[3], args[4], args[5]);
-                break;
-            case "add-comment":
-                addComment(args[0], args[1], args[2], args[3], args[4]);
-                break;
-            case "search":
-                search(args[0]);
-                break;
-            case "search-user":
-                searchUser(args[0], args[1], args[2]);
-                break;
-            case "report-penalties-sum":
-                reportPenalties(args[0], args[1]);
-                break;
-            case "report-passed-deadline":
-                reportPassedDeadLine(args[0], args[1], args[2], args[3], args[4]);
-                break;
-            case "library-report":
-                libraryReport(args[0], args[1], args[2]);
-                break;
-            case "category-report":
-                categoryReport(args[0], args[1], args[2], args[3]);
-                break;
-            case "report-most-popular":
-                reportMostPopular(args[0], args[1], args[2]);
-                break;
-            case "report-sell":
-                reportSelling(args[0], args[1], args[2]);
-                break;
+        router.routing(command[0],args);
+    }
+
+    public void addLibrary(String[] args) {
+        Library library = new Library(args[2], args[3], args[4], Integer.parseInt(args[5]), args[6]);
+        System.out.println(center.addLibrary(args[0], args[1], library));
+    }
+
+    public void addCategory(String[] args) {
+        Category category = new Category(args[2], args[3], args[4]);
+        System.out.println(center.addCategory(args[0], args[1], category));
+    }
+
+    public void addStudent(String[] args) {
+        Student student = new Student(args[2], args[3], args[4], args[5], args[6], args[7], args[8]);
+        System.out.println(center.addUser(args[0], args[1], student));
+    }
+
+    public void addStaff(String[] args) {
+        if (args[9].equals("staff")) {
+            Staff staff = new Staff(args[2], args[3], args[4], args[5], args[6], args[7], args[8]);
+            System.out.println(center.addUser(args[0], args[1], staff));
+        } else {
+            Professor professor = new Professor(args[2], args[3], args[4], args[5], args[6], args[7], args[8]);
+            System.out.println(center.addUser(args[0], args[1], professor));
         }
     }
 
-    public void addLibrary(String adminId, String adminPass, String id, String name, String year, int numDesk, String address) {
-        Library library = new Library(id, name, year, numDesk, address);
-        System.out.println(center.addLibrary(adminId, adminPass, library));
+    public void addManager(String[] args) {
+        Manager manager = new Manager(args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9]);
+        System.out.println(center.addUser(args[0], args[1], manager));
     }
 
-    public void addCategory(String adminId, String adminPass, String id, String name, String parentId) {
-        Category category = new Category(id, name, parentId);
-        System.out.println(center.addCategory(adminId, adminPass, category));
-
+    public void removeUser(String[] args) {
+        System.out.println(center.removeUser(args[0], args[1], args[2]));
     }
 
-    public void addStudent(String adminId, String adminPass, String id, String pass, String firstName, String lastName, String nationalId, String year, String address) {
-        Student student = new Student(id, pass, firstName, lastName, nationalId, year, address);
-        System.out.println(center.addUser(adminId, adminPass, student));
+    public void addBook(String[] args) {
+        Book book = new Book(args[2], args[3], args[4], args[8], args[9], args[5], Integer.parseInt(args[7]), args[6]);
+        System.out.println(center.addResource(args[0], args[1], book));
     }
 
-    public void addStaff(String adminId, String adminPass, String id, String pass, String firstName, String lastName, String nationalId, String year, String address, String type) {
-        if (type.equals("staff")) {
-            Staff staff = new Staff(id, pass, firstName, lastName, nationalId, year, address);
-            System.out.println(center.addUser(adminId, adminPass, staff));
-            return;
-        }
-        Professor professor = new Professor(id, pass, firstName, lastName, nationalId, year, address);
-        System.out.println(center.addUser(adminId, adminPass, professor));
+    public void addThesis(String[] args) {
+        Thesis thesis = new Thesis(args[2], args[3], args[4], args[7], args[8], args[4], args[6]);
+        System.out.println(center.addResource(args[0], args[1], thesis));
     }
 
-    public void addManager(String adminId, String adminPass, String id, String pass, String firstName, String lastName, String nationalId, String year, String address, String libraryId) {
-        Manager manager = new Manager(id, pass, firstName, lastName, nationalId, year, address, libraryId);
-        System.out.println(center.addUser(adminId, adminPass, manager));
-
+    public void addGanjine(String[] args) {
+        GanjineBook ganjineBook = new GanjineBook(args[2], args[3], args[4], args[8], args[9], args[5], args[6], args[7]);
+        System.out.println(center.addResource(args[0], args[1], ganjineBook));
     }
 
-    public void removeUser(String adminId, String adminPass, String id) {
-        System.out.println(center.removeUser(adminId, adminPass, id));
+    public void addSellingBook(String[] args) {
+        SellingBook sellingBook = new SellingBook(args[2], args[3], args[4], args[10], args[11], args[5], Integer.parseInt(args[7]), args[6], args[8], args[9]);
+        System.out.println(center.addResource(args[0], args[1], sellingBook));
     }
 
-    public void addBook(String managerId, String managerPass, String id, String subject, String authorName, String publisher, String year, int number, String categoryId, String libraryId) {
-        Book book = new Book(id, subject, authorName, categoryId, libraryId, publisher, number, year);
-        System.out.println(center.addResource(managerId, managerPass, book));
+    public void removeResource(String[] args) {
+        System.out.println(center.removeResource(args[0], args[1], args[2], args[3]));
     }
 
-    public void addThesis(String managerId, String managerPass, String id, String subject, String authorName, String advisor, String defYear, String categoryId, String libraryId) {
-        Thesis thesis = new Thesis(id, subject, authorName, categoryId, libraryId, advisor, defYear);
-        System.out.println(center.addResource(managerId, managerPass, thesis));
+    public void borrow(String[] args) throws ParseException {
+        Date date = new SimpleDateFormat("yyyy-MM-dd hh:mm").parse(args[4] + " " + args[5]);
+        Borrow borrow = new Borrow(args[0], args[3], args[2], date);
+        System.out.println(center.borrow(borrow, args[1]));
     }
 
-    public void addGanjine(String managerId, String managerPass, String id, String subject, String authorName, String publisher, String year, String donor, String categoryId, String libraryId) {
-        GanjineBook ganjineBook = new GanjineBook(id, subject, authorName, categoryId, libraryId, publisher,year, donor);
-        System.out.println(center.addResource(managerId, managerPass, ganjineBook));
+    public void returning(String[] args) throws ParseException {
+        Date date = new SimpleDateFormat("yyyy-MM-dd hh:mm").parse(args[4] + " " + args[5]);
+        Borrow borrow = new Borrow(args[0], args[3], args[2], date);
+        System.out.println(center.returning(borrow, args[1]));
     }
 
-    public void addSellingBook(String managerId, String managerPass, String id, String subject, String authorName, String publisher, String year, int number, String price, String discount, String categoryId, String libraryId) {
-        SellingBook sellingBook = new SellingBook(id, subject, authorName, categoryId, libraryId, publisher, number, year, price, discount);
-        System.out.println(center.addResource(managerId, managerPass, sellingBook));
+    public void buy(String[] args) {
+        System.out.println(center.buy(args[0], args[1], args[2], args[3]));
     }
 
-    public void removeResource(String managerId, String managerPass, String resourceId, String libraryId) {
-        System.out.println(center.removeResource(managerId, managerPass, resourceId, libraryId));
+    public void read(String[] args) throws ParseException {
+        Date date = new SimpleDateFormat("yyyy-MM-dd hh:mm").parse(args[4] + " " + args[5]);
+        System.out.println(center.read(args[0], args[1], args[2], args[3], date));
     }
 
-    public void borrow(String userId, String userPass, String libraryId, String resourceId, String strDate, String hour) throws ParseException {
-        Date date = new SimpleDateFormat("yyyy-MM-dd hh:mm").parse(strDate + " " + hour);
-        Borrow borrow = new Borrow(userId, resourceId, libraryId, date);
-        System.out.println(center.borrow(borrow, userPass));
+    public void addComment(String[] args) {
+        System.out.println(center.addComment(args[0], args[1], args[2], args[3], args[4]));
     }
 
-    public void returning(String userId, String userPass, String libraryId, String resourceId, String strDate, String hour) throws ParseException {
-        Date date = new SimpleDateFormat("yyyy-MM-dd hh:mm").parse(strDate + " " + hour);
-        Borrow borrow = new Borrow(userId, resourceId, libraryId, date);
-        System.out.println(center.returning(borrow, userPass));
-    }
-    public void buy(String userId, String pass, String libraryId, String resourceId) {
-        System.out.println(center.buy(userId, pass, libraryId, resourceId));
+    public void search(String[] args) {
+        System.out.println(center.search(args[0]));
     }
 
-    public void read(String userId, String pass, String libraryId, String resourceId, String strDate, String hour) throws ParseException {
-        Date date = new SimpleDateFormat("yyyy-MM-dd hh:mm").parse(strDate + " " + hour);
-        System.out.println(center.read(userId, pass, libraryId, resourceId, date));
+    public void searchUser(String[] args) {
+        System.out.println(center.searchUser(args[0], args[1], args[2]));
     }
 
-    public void addComment(String userId,String pass,String libraryId,String bookId,String comment) {
-        System.out.println(center.addComment(userId, pass, libraryId, bookId, comment));
+    public void reportPenalties(String[] args) {
+        System.out.println(center.reportPenalties(args[0], args[1]));
     }
 
-    public void search(String key) {
-        System.out.println(center.search(key));
+    public void reportPassedDeadLine(String[] args) throws ParseException {
+        Date date = new SimpleDateFormat("yyyy-MM-dd hh:mm").parse(args[3] + " " + args[4]);
+        System.out.println(center.reportPassedDeadLine(args[0], args[1], args[2], date));
     }
 
-    public void searchUser(String userId, String pass, String key) {
-        System.out.println(center.searchUser(userId, pass, key));
+    public void libraryReport(String[] args) {
+        System.out.println(center.libraryReport(args[0], args[1], args[2]));
     }
 
-    public void reportPenalties(String userId, String pass) {
-        System.out.println(center.reportPenalties(userId, pass));
+    public void categoryReport(String[] args) {
+        System.out.println(center.categoryReport(args[0], args[1], args[2], args[3]));
     }
 
-    public void reportPassedDeadLine(String userId, String pass, String libraryId, String strDate, String hour) throws ParseException {
-        Date date = new SimpleDateFormat("yyyy-MM-dd hh:mm").parse(strDate + " " + hour);
-        System.out.println(center.reportPassedDeadLine(userId, pass, libraryId, date));
+    public void reportMostPopular(String[] args) {
+        System.out.println(center.reportMostPopular(args[0], args[1], args[2]));
     }
 
-    public void libraryReport(String userId, String pass, String libraryId) {
-        System.out.println(center.libraryReport(userId, pass, libraryId));
-    }
-
-    public void categoryReport(String userId, String pass, String categoryId, String libraryId) {
-        System.out.println(center.categoryReport(userId, pass, categoryId, libraryId));
-    }
-
-    public void reportMostPopular(String userId,String pass,String libraryId) {
-        System.out.println(center.reportMostPopular(userId, pass, libraryId));
-    }
-
-    public void reportSelling(String userId, String pass, String libraryId) {
-        System.out.println(center.reportSelling(userId, pass, libraryId));
+    public void reportSelling(String[] args) {
+        System.out.println(center.reportSelling(args[0], args[1], args[2]));
     }
 }
 
