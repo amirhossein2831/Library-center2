2f43bee17bcdceaf3deb3630da80f4f502fd2266
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-03-13 14:32:06 +0330

    add all routes

--- src/com/Library/Compunent/Exception/Route.java
+++ src/com/Library/Compunent/Exception/Route.java
@@ -12,6 +12,29 @@ public class Route {
     public void fetchRoute() {
         route = new HashMap<>() {{
             put("add-library", "addLibrary");
+            put("add-category", "addCategory");
+            put("add-student", "addStudent");
+            put("add-staff", "addStaff");
+            put("add-manager", "addManager");
+            put("remove-user", "removeUser");
+            put("add-book", "addBook");
+            put("add-thesis", "addThesis");
+            put("add-ganjineh-book", "addGanjine");
+            put("add-selling-book", "addSellingBook");
+            put("remove-resource", "removeResource");
+            put("borrow", "borrow");
+            put("return", "returning");
+            put("buy", "buy");
+            put("read", "read");
+            put("add-comment", "addComment");
+            put("search", "search");
+            put("search-user", "searchUser");
+            put("report-penalties-sum", "reportPenalties");
+            put("report-passed-deadline", "reportPassedDeadLine");
+            put("library-report", "libraryReport");
+            put("category-report", "categoryReport");
+            put("report-most-popular", "reportMostPopular");
+            put("report-sell", "reportSelling");
         }};
     }
 
