22575376b432825cee6081e89fdb8cbb80053a6a
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-03-13 15:29:40 +0330

    update the main and catch some exception

--- src/com/Library/Main.java
+++ src/com/Library/Main.java
@@ -5,21 +5,25 @@ import com.Library.Compunent.Exception.NotFoundException;
 import com.Library.Compunent.Exception.PermissionDeniedException;
 import com.Library.Compunent.Parser.Parser;
 
+import java.lang.reflect.InvocationTargetException;
 import java.text.ParseException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String input = "";
         Parser parser = new Parser();
+        String input = scanner.nextLine();
         while (!input.equals("finish")) {
-            input = scanner.nextLine();
             try {
                 parser.parsCommand(input);
             } catch (NotFoundException | InvalidPassException | PermissionDeniedException e) {
                 System.out.println(e.getMessage());
+            } catch (ClassNotFoundException | InvocationTargetException | NoSuchMethodException |
+                     InstantiationException | IllegalAccessException e) {
+                throw new RuntimeException(e);
             }
+            input = scanner.nextLine();
         }
     }
 }
