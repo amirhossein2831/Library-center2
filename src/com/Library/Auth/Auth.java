3170f57e366dedf3edb1a93c8968a5a9b278530d
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-03-28 01:10:52 +0330

    change the auth class

--- src/com/Library/Auth/Auth.java
+++ src/com/Library/Auth/Auth.java
@@ -1,55 +1,7 @@
 package com.Library.Auth;
 
-import com.Library.Compunent.Exception.InvalidPassException;
-import com.Library.Compunent.Exception.NotFoundException;
-import com.Library.Compunent.Exception.PermissionDeniedException;
-import com.Library.Models.LibraryCenter;
-import com.Library.Models.Users.Admin;
-import com.Library.Models.Users.Manager;
+import java.util.HashMap;
 
-public class Auth {
-    public static void adminAuth(String id, String pass) {
-        if (LibraryCenter.getUsers().get(id) == null) {
-            throw new NotFoundException();
-        } else if (!(LibraryCenter.getUsers().get(id) instanceof Admin)) {
-            throw new PermissionDeniedException();
-        }
-        if (!LibraryCenter.getUsers().get(id).getPass().equals(pass)) {
-            throw new InvalidPassException();
-        }
-    }
-
-    public static void managerAuth(String id, String pass) {
-        if (LibraryCenter.getUsers().get(id) == null) {
-            throw new NotFoundException();
-        } else if (!LibraryCenter.getUsers().get(id).getPass().equals(pass)) {
-            throw new InvalidPassException();
-        }
-        if (!(LibraryCenter.getUsers().get(id) instanceof Manager)) {
-            throw new PermissionDeniedException();
-        }
-    }
-
-    public static void libraryManagerAuth(String id,String libraryId) {
-        if (LibraryCenter.getLibraries().get(libraryId) == null) {
-            throw new NotFoundException();
-        }
-        if (!((Manager) LibraryCenter.getUsers().get(id)).getLibraryId().equals(libraryId)) {
-            throw new PermissionDeniedException();
-        }
-    }
-
-    public static void featureAuth(String id, String pass,String libraryId, String resourceId) {
-        if (LibraryCenter.getUsers().get(id) == null) {
-            throw new NotFoundException();
-        } else if (!LibraryCenter.getUsers().get(id).getPass().equals(pass)) {
-            throw new InvalidPassException();
-        }
-        if (LibraryCenter.getLibraries().get(libraryId) == null) {
-            throw new NotFoundException();
-        }
-        if (LibraryCenter.getLibraries().get(libraryId).getResource(resourceId) == null) {
-            throw new NotFoundException();
-        }
-    }
+public abstract class Auth {
+    public abstract void authenticate(HashMap<String, String> args);
 }
