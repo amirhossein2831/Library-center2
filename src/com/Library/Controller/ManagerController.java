82a4d5bc238576434d98b90d68f28809b76c82ce
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-03-28 03:53:46 +0330

    update manager controller and fix sum bug

--- src/com/Library/Controller/ManagerController.java
+++ src/com/Library/Controller/ManagerController.java
@@ -1,6 +1,7 @@
 package com.Library.Controller;
 
 import com.Library.Auth.Auth;
+import com.Library.Auth.ManagerAuth;
 import com.Library.Models.Library;
 import com.Library.Models.LibraryCenter;
 import com.Library.Models.Resources.*;
@@ -12,31 +13,32 @@ import java.util.Date;
 public class ManagerController extends BaseController {
 
     public String addBook(String[] args) {
+
         Book book = new Book(args[2], args[3], args[4], args[8], args[9], args[5], Integer.parseInt(args[7]), args[6]);
-        Auth.libraryManagerAuth(args[0], book.getLibraryId());
+        ManagerAuth.libraryManagerAuth(args[0], book.getLibraryId());
         return checkResource(book);
     }
 
     public String addThesis(String[] args) {
-        Thesis thesis = new Thesis(args[2], args[3], args[4], args[7], args[8], args[4], args[6]);
-        Auth.libraryManagerAuth(args[0], thesis.getLibraryId());
+        Thesis thesis = new Thesis(args[2], args[3], args[4], args[7], args[8], args[5], args[6]);
+        ManagerAuth.libraryManagerAuth(args[0], thesis.getLibraryId());
         return checkResource(thesis);
     }
 
     public String addGanjine(String[] args) {
         GanjineBook ganjineBook = new GanjineBook(args[2], args[3], args[4], args[8], args[9], args[5], args[6], args[7]);
-        Auth.libraryManagerAuth(args[0], ganjineBook.getLibraryId());
+        ManagerAuth.libraryManagerAuth(args[0], ganjineBook.getLibraryId());
         return checkResource(ganjineBook);
     }
 
     public String addSellingBook(String[] args) {
         SellingBook sellingBook = new SellingBook(args[2], args[3], args[4], args[10], args[11], args[5], Integer.parseInt(args[7]), args[6], args[8], args[9]);
-        Auth.libraryManagerAuth(args[0], sellingBook.getLibraryId());
+        ManagerAuth.libraryManagerAuth(args[0], sellingBook.getLibraryId());
         return checkResource(sellingBook);
     }
 
     public String removeResource(String[] args) {
-        Auth.libraryManagerAuth(args[0], args[3]);
+        ManagerAuth.libraryManagerAuth(args[0], args[3]);
         Library library = LibraryCenter.getLibraries().get(args[3]);
         if (library.getResources().get(args[2]) == null) {
             return "not-found";
@@ -65,20 +67,20 @@ public class ManagerController extends BaseController {
 
     public String reportPassedDeadLine(String[] args) throws ParseException {
         Date date = new SimpleDateFormat("yyyy-MM-dd hh:mm").parse(args[3] + " " + args[4]);
-        Library library = LibraryCenter.getLibraries().get(args[3]);
-        Auth.libraryManagerAuth(args[0], args[2]);
+        Library library = LibraryCenter.getLibraries().get(args[2]);
+        ManagerAuth.libraryManagerAuth(args[0], args[2]);
         return library.reportPassedDeadLine(date, LibraryCenter.getUsers());
     }
 
     public String libraryReport(String[] args) {
-        Library library = LibraryCenter.getLibraries().get(args[3]);
-        Auth.libraryManagerAuth(args[0], args[2]);
+        Library library = LibraryCenter.getLibraries().get(args[2]);
+        ManagerAuth.libraryManagerAuth(args[0], args[2]);
         return library.libraryReport();
     }
 
     public String categoryReport(String[] args) {
         Library library = LibraryCenter.getLibraries().get(args[3]);
-        Auth.libraryManagerAuth(args[0], args[3]);
+        ManagerAuth.libraryManagerAuth(args[0], args[3]);
         int[] hold = library.categoryReport(LibraryCenter.getCategories(), args[2]);
         if (hold == null) {
             return "not-found";
@@ -88,13 +90,13 @@ public class ManagerController extends BaseController {
 
     public String reportMostPopular(String[] args) {
         Library library = LibraryCenter.getLibraries().get(args[3]);
-        Auth.libraryManagerAuth(args[0], args[2]);
+        ManagerAuth.libraryManagerAuth(args[0], args[2]);
         return library.reportMostPopular();
     }
 
     public String reportSelling(String[] args) {
         Library library = LibraryCenter.getLibraries().get(args[3]);
-        Auth.libraryManagerAuth(args[0], args[2]);
+        ManagerAuth.libraryManagerAuth(args[0], args[2]);
         return library.reportSelling(LibraryCenter.getUsers(), args[2]);
     }
 }
